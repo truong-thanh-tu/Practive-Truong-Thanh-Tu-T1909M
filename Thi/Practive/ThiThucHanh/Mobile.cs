63921f49591ff1beb4fbd07c1a5eefbd200a9eea
= <60811767+truong-thanh-tu@users.noreply.github.com>
2020-06-22 14:42:52 +0700

    Thi Thuc  Hanh Truong Thanh Tu

---
+++ Thi/Practive/ThiThucHanh/Mobile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practive.ThiThucHanh
+{
+    class Mobile:Product
+    {
+        public Mobile(int id, string name, double price, string producert) : base(id, name, price, producert)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Price = price;
+            this.Producert = producert;
+        }
+        public override double ComputeTax(float x)
+        {
+            return this.Price = this.Price * x;
+        }
+        public override void Show()
+        {
+            Console.WriteLine("ID: {0}", this.ID);
+            Console.WriteLine("Name: {0}", this.Name);
+            Console.WriteLine("Price: {0}", this.Price);
+            Console.WriteLine("Producet: {0}", this.Producert);
+
+        }
+    }
+}
